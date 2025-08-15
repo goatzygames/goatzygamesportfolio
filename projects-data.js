698f4a1bc89faa6bd774418447219c257a494182
goatzygames <7hamsterman7@gmail.com>
2025-08-15 22:44:52 +0300

    2.0

--- projects-data.js
+++ projects-data.js
@@ -6,21 +6,21 @@ window.projects = [
     type: "game dev",
     price: 0,
     description: "Flappy VHS is an 1 Day Project which I singlehandedly with some assets and textures borrowed from sketchfab and google made, just to show what I can make in a single day. I do not plan to really continue this.",
-    videoUrl: "https://www.youtube.com/embed/VIDEO_ID",
+    videoUrl: "https://www.youtube.com/embed/xMnPQxotlio",
     images: [
-      "images/flappy1.png",
-      "images/flappy2.png",
-      "images/flappy3.png"
+      "pics/flappyvhs-image1.png",
+      "pics/flappyvhs-image2.png",
+      "pics/flappyvhs-image3.png"
     ],
-	downloadUrl: "files/my-cool-project.zip"
+	downloadUrl: "https://drive.google.com/file/d/1O5K4_76QB5_tsD1SdasyLQnY-DndHd3x/view?usp=sharing"
   },
   {
     name: "Portfolio Website",
     tags: ["web dev", "portfolio"],
-    date: "2025-05-10",
+    date: "2025-08-15",
     type: "web dev",
     price: 0,
-    description: "A sleek, modern portfolio site showcasing my work in web development.",
+    description: "This is the website you're currently on, this is my proudest web development work yet.",
     videoUrl: "",
     images: ["pics/goatzy.games-image1.png"],
 	downloadUrl: "https://www.goatzy.games"
@@ -28,7 +28,7 @@ window.projects = [
   {
     name: "The Last Customer",
     tags: ["the", "last", "customer", "horror"],
-    date: "2025-08-14",
+    date: "2024-10-25",
     type: "game dev",
     price: 5,
     description: "The Last Customer is a survival horror game where you play as a gas station clerk facing nightmarish creatures disguised as customers. Make quick decisions, manage resources, and uncover dark secrets to survive. Test your instincts and unravel the mystery before it's too late.<br><br>This also is my first big project ever, so I am not saying this is my proudest work ever, but it really helped me learn game dev more as it has a lot of different mechanics I think no one has yet discovered. ",
@@ -39,5 +39,35 @@ window.projects = [
 	  "pics/thelastcustomer-image3.jpg"
     ],
 	downloadUrl: "https://store.steampowered.com/app/3249630/The_Last_Customer"
+  },
+  {
+    name: "+1 Speed Per Second",
+    tags: ["plus", "one", "speed", "second"],
+    date: "2025-01-13",
+    type: "game dev",
+    price: 0,
+    description: "<em>+1 Speed Per Second</em> is a scrap project I made earlier this year as a scrap project just to try making different mechanisms to replicate a <em>ROBLOX</em> game which is somewhat similar to what I made.",
+    videoUrl: "",
+    images: [
+      "pics/plusonesps-image1.png",
+      "pics/plusonesps-image2.png",
+	  "pics/plusonesps-image3.png"
+    ],
+	downloadUrl: "contact.html"
+  },
+  {
+    name: "Invert Nest",
+    tags: ["invert", "nest"],
+    date: "2025-07-09",
+    type: "web dev",
+    price: 0,
+    description: "A web development project I made mainly for fun to try to create this cool local marketplace for my area for selling animal supplies, and as the name suggests, mainly pet invertebrate supplies. It really didn't caught on as well as I expected honestly. The website is <strong>not</strong> being maintained and does not work at the moment. ",
+    videoUrl: "",
+    images: [
+      "pics/invertnest-image1.png",
+      "pics/invertnest-image2.png",
+	  "pics/invertnest-image3.png"
+    ],
+	downloadUrl: "https://invertnest.eu"
   }
 ];
